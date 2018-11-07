51b487533e53e64b78d5b21db257d7d942a46245
Rebecca Carter <rebecca.carter77@gmail.com>
2018-11-07 21:27:08 +1100

    7/11/18- 9:30pm

--- Coursera/test.py
+++ Coursera/test.py
@@ -1,5 +1,1 @@
-import urllib.request, urllib.parse, urllib.error
 
-fhand=urllib.request.urlopen('https://docs.google.com/forms/d/1i0T8Tl2dsATvADtcuecDUGfcq6y3aNiZ_RBwQATogPg/edit#responses')
-for line in fhand:
-    print(line.decode().strip())
